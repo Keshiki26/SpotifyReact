6224e8bfeed19af4c8fcfe336c058fbd12e7740e
keshiki26 <103056475@student.swin.edu.au>
2020-12-16 18:24:58 +1100

    p1

--- src/YoutubeAPI.js
+++ src/YoutubeAPI.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
+//AIzaSyBiKnJjtRdHvnw6Liq8yycELIT2glYQMqM
+
 export default axios.create({
 	baseURL: 'https://www.googleapis.com/youtube/v3',
 	params: {
 		maxResults: 3,
-		key: 'AIzaSyBiKnJjtRdHvnw6Liq8yycELIT2glYQMqM',
+		key: 'AIzaSyB9lDUmAOwhcxiIj9MrXBlUK8yJzLk-KI4',
 		part: 'snippet',
 	},
 });
