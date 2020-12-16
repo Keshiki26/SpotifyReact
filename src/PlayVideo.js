b9f0aec754fd9b7c3783bb9d1ebd3f489f060141
keshiki26 <103056475@student.swin.edu.au>
2020-12-16 21:07:04 +1100

    v1 c

--- src/PlayVideo.js
+++ src/PlayVideo.js
@@ -27,7 +27,9 @@ const PlayVideo = (props) => {
 					{props.video.map((video, index) => {
 						return (
 							<div className="playerContents">
-								<h2>{video.snippet.title}</h2>
+								<a href={`https://www.youtube.com/watch?v=${video.id.videoId}`}>
+									<h2>{video.snippet.title}</h2>
+								</a>
 								<p>{video.snippet.description}</p>
 							</div>
 						);
